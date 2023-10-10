8df02f471a7b25b7aedfabc6f237e74e24b2a594
taekklee <taekklee@student.42tokyo.jp>
2023-10-10 19:19:39 +0900

    [fix] norminette

--- lib/libcolor/libcolor.h
+++ lib/libcolor/libcolor.h
@@ -1,23 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   color_create.c                                     :+:      :+:    :+:   */
+/*   libcolor.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: taekklee <taekklee@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/10 18:31:35 by taekklee          #+#    #+#             */
-/*   Updated: 2023/10/10 19:19:25 by taekklee         ###   ########.fr       */
+/*   Created: 2023/10/10 19:18:36 by taekklee          #+#    #+#             */
+/*   Updated: 2023/10/10 19:19:07 by taekklee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libcolor.h"
+#ifndef LIBCOLOR_H
+# define LIBCOLOR_H
 
-t_color	color_create(double red, double green, double blue)
+typedef struct s_color
 {
-	t_color	new;
+	double	red;
+	double	green;
+	double	blue;
+}	t_color;
 
-	new.red = red;
-	new.green = green;
-	new.blue = blue;
-	return (new);
-}
+t_color			color_create(double red, double green, double blue);
+unsigned int	color_cast(t_color color);
+
+#endif
