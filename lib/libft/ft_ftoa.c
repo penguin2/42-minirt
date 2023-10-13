0be46858e627b5523eb6f73c37b8318396c59d31
rikeda-cloud <longsichitian648@gmail.com>
2023-10-13 16:18:54 +0900

    [fix] ft_ftoa関数のlimitで小数部を制限する

--- lib/libft/ft_ftoa.c
+++ lib/libft/ft_ftoa.c
@@ -6,7 +6,7 @@
 /*   By: rikeda <rikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 00:12:52 by rikeda            #+#    #+#             */
-/*   Updated: 2023/10/13 15:54:43 by rikeda           ###   ########.fr       */
+/*   Updated: 2023/10/13 16:16:40 by rikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,19 @@ char	*ft_ftoa(double number, size_t limit)
 	char	*str_integer;
 	size_t	idx;
 
-	str = (char *)ft_xcalloc(limit + 1, sizeof(char));
 	str_integer = ft_ltoa((long int)number);
-	idx = ft_strlcpy(str, str_integer, limit + 1);
+	idx = ft_strlen(str_integer);
+	str = (char *)ft_xcalloc(idx + limit + 2, sizeof(char));
+	ft_strlcpy(str, str_integer, idx + 1);
 	free(str_integer);
-	if (limit <= idx)
-		return (str);
 	str[idx++] = '.';
 	number -= (long int)number;
-	while (idx < limit)
+	while (0 < limit)
 	{
+		limit--;
 		number *= 10;
 		str[idx++] = ((long int)number % 10) + '0';
+		number -= (long int)number;
 	}
 	return (str);
 }
