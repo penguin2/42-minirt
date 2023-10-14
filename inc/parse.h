7139c5fb7a854f8eff2e6a5a586eca9b86bfb05c
rikeda-cloud <longsichitian648@gmail.com>
2023-10-14 19:01:40 +0900

    [add] get_next_lineの追加 未完成のcheck_json_format.cの追加

--- inc/parse.h
+++ inc/parse.h
@@ -6,7 +6,7 @@
 /*   By: rikeda <rikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 18:39:58 by rikeda            #+#    #+#             */
-/*   Updated: 2023/10/14 16:32:53 by rikeda           ###   ########.fr       */
+/*   Updated: 2023/10/14 18:57:13 by rikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,23 @@
 # define PARSE_H
 
 # include <stddef.h>
+
+typedef struct s_dict
+{
+	char	*key;
+	int		type;
+	void	*value;
+}	t_dict;
+
+enum
+{
+	NOT_SET,
+	NUMBER,
+	BOOL,
+	STRING,
+	DICT,
+	LIST,
+};
 
 int		check_extention(const char *file, const char *extention);
 char	*del_commentout(const char *str, const char *commentout_str);
