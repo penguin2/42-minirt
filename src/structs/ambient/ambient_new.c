eeac44d4e2e382c6aa3065757645c951abd9d04f
taekklee <taekklee@student.42tokyo.jp>
2023-10-15 23:41:17 +0900

    [fix] new with ft_xcalloc in ambient_new

--- src/structs/ambient/ambient_new.c
+++ src/structs/ambient/ambient_new.c
@@ -6,16 +6,18 @@
 /*   By: taekklee <taekklee@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 16:30:47 by taekklee          #+#    #+#             */
-/*   Updated: 2023/10/12 16:31:23 by taekklee         ###   ########.fr       */
+/*   Updated: 2023/10/15 20:40:22 by taekklee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ambient.h"
+#include "libft.h"
 
 t_ambient	*ambient_new(double brightness, t_color color)
 {
 	t_ambient	*new;
 
+	new = ft_xcalloc(1, sizeof(t_ambient));
 	new->brightness = brightness;
 	new->color = color;
 	return (new);
