79bd29072cf1452e8a4f2d583d3cc2296dd7fb45
rikeda <rikeda@penguin>
2023-10-15 18:11:19 +0900

    [fix] del_commentout コメントアウトがある場合、引数で渡された文字列を関数内でfreeし、新しい文字列を返すように変更

--- src/parse/utils/del_commentout.c
+++ src/parse/utils/del_commentout.c
@@ -6,19 +6,24 @@
 /*   By: rikeda <rikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 18:15:37 by rikeda            #+#    #+#             */
-/*   Updated: 2023/10/13 19:22:41 by rikeda           ###   ########.fr       */
+/*   Updated: 2023/10/15 18:09:25 by rikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*del_commentout(const char *str, const char *commentout_str)
+char	*del_commentout(char *str, const char *commentout_str)
 {
-	char	*commentout_start;
+	const char	*commentout_start;
+	char		*new_str;
 
 	commentout_start = ft_strstr(str, commentout_str);
 	if (commentout_start != NULL)
-		return (ft_strdup_n(str, (commentout_start - str)));
+	{
+		new_str = ft_strdup_n(str, (commentout_start - str));
+		free(str);
+		return (new_str);
+	}
 	else
-		return (ft_strdup(str));
+		return (str);
 }
