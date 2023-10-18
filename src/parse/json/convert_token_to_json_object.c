d4d656bf1c9da0219ead4091bbd08e2e1b8a6de0
rikeda-cloud <longsichitian648@gmail.com>
2023-10-18 20:46:42 +0900

    [fix] norm

--- src/parse/json/convert_token_to_json_object.c
+++ src/parse/json/convert_token_to_json_object.c
@@ -6,7 +6,7 @@
 /*   By: rikeda <rikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 15:46:40 by rikeda            #+#    #+#             */
-/*   Updated: 2023/10/18 15:55:21 by rikeda           ###   ########.fr       */
+/*   Updated: 2023/10/18 20:40:40 by rikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ t_vla	*convert_token_to_json_object(t_vla *token)
 {
 	t_vla	*json_object;
 
-	json_object =  ft_vla_new();
-
+	json_object = ft_vla_new();
 	return (json_object);
 }
