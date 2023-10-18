718462be2767bf1d2f03c7be766cee8efa5c3f29
rikeda-cloud <longsichitian648@gmail.com>
2023-10-18 20:39:25 +0900

    [add] dict関連の関数を追加

--- src/parse/json/convert_token_to_json_object.c
+++ src/parse/json/convert_token_to_json_object.c
@@ -1,23 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   dict_new.c                                         :+:      :+:    :+:   */
+/*   convert_token_to_json_object.c                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rikeda <rikeda@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rikeda <rikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/18 20:20:28 by rikeda            #+#    #+#             */
-/*   Updated: 2023/10/25 14:33:22 by rikeda           ###   ########.fr       */
+/*   Created: 2023/10/18 15:46:40 by rikeda            #+#    #+#             */
+/*   Updated: 2023/10/18 15:55:21 by rikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "parse.h"
+#include "libft.h"
 
-t_dict	*dict_new(const char *key, void *value)
+t_vla	*convert_token_to_json_object(t_vla *token)
 {
-	t_dict	*dict;
+	t_vla	*json_object;
 
-	dict = (t_dict *)ft_xcalloc(1, sizeof(t_dict));
-	dict->key = ft_strdup(key);
-	dict->value = value;
-	return (dict);
+	json_object =  ft_vla_new();
+
+	return (json_object);
 }
