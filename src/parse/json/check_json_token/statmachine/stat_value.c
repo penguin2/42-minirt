ab359dbcda9ee71e0d91d1939bcf69bbca7da60e
rikeda-cloud <longsichitian648@gmail.com>
2023-10-18 15:17:20 +0900

    [fix] is_closedのバグを修正、それに伴って呼び出す側の関数も修正

--- src/parse/json/check_json_token/statmachine/stat_value.c
+++ src/parse/json/check_json_token/statmachine/stat_value.c
@@ -6,7 +6,7 @@
 /*   By: rikeda <rikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 19:40:13 by rikeda            #+#    #+#             */
-/*   Updated: 2023/10/18 12:11:45 by rikeda           ###   ########.fr       */
+/*   Updated: 2023/10/18 15:11:06 by rikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ int	stat_value(t_vla *token, t_vla *stack, size_t idx, int stat)
 	if (idx == token->size || stat == END)
 		return (stat_check_end(token, idx, stat));
 	str = (char *)token->array[idx];
-	if (stat == IN_DICT && *str == '}' && is_closed(stack, '{'))
+	if (stat == IN_DICT && *str == '}' && is_closed(stack, '}'))
 		return (stat_dict_end(token, stack, (idx + 1), stat_get(stack)));
-	else if (stat == IN_LIST && *str == ']' && is_closed(stack, '['))
+	else if (stat == IN_LIST && *str == ']' && is_closed(stack, ']'))
 		return (stat_list_end(token, stack, (idx + 1), stat_get(stack)));
 	else if (*str == ',')
 		return (stat_comma(token, stack, (idx + 1), stat));
