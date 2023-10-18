7b657b5f4aeb3ec284d4ad2687ebc9ad59208071
rikeda-cloud <longsichitian648@gmail.com>
2023-10-18 15:17:20 +0900

    [fix] is_closedのバグを修正、それに伴って呼び出す側の関数も修正

--- src/parse/json/check_json_token/statmachine/is_closed.c
+++ src/parse/json/check_json_token/statmachine/is_closed.c
@@ -6,17 +6,17 @@
 /*   By: rikeda <rikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 12:05:04 by rikeda            #+#    #+#             */
-/*   Updated: 2023/10/18 12:07:39 by rikeda           ###   ########.fr       */
+/*   Updated: 2023/10/18 15:09:13 by rikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
 
-bool	is_closed(t_vla *stack, int open_char)
+bool	is_closed(t_vla *stack, int close_char)
 {
-	int	close_char;
+	int	open_char;
 
-	close_char = *(char *)ft_vla_pop(stack);
+	open_char = *(char *)ft_vla_pop(stack);
 	if (open_char == '{' && close_char == '}')
 		return (true);
 	else if (open_char == '[' && close_char == ']')
--- src/parse/json/check_json_token/statmachine/stat_dict_end.c
+++ src/parse/json/check_json_token/statmachine/stat_dict_end.c
@@ -6,7 +6,7 @@
 /*   By: rikeda <rikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 18:52:57 by rikeda            #+#    #+#             */
-/*   Updated: 2023/10/18 12:10:53 by rikeda           ###   ########.fr       */
+/*   Updated: 2023/10/18 15:10:30 by rikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ int	stat_dict_end(t_vla *token, t_vla *stack, size_t idx, int stat)
 	if (idx == token->size || stat == END)
 		return (stat_check_end(token, idx, stat));
 	str = (char *)token->array[idx];
-	if (stat == IN_DICT && *str == '}' && is_closed(stack, '{'))
+	if (stat == IN_DICT && *str == '}' && is_closed(stack, '}'))
 		return (stat_dict_end(token, stack, (idx + 1), stat_get(stack)));
-	else if (stat == IN_LIST && *str == ']' && is_closed(stack, '['))
+	else if (stat == IN_LIST && *str == ']' && is_closed(stack, ']'))
 		return (stat_list_end(token, stack, (idx + 1), stat_get(stack)));
 	else if (*str == ',')
 		return (stat_comma(token, stack, (idx + 1), stat));
