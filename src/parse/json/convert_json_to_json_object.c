a78262bff61061b3f79b47bffa227a01cb653a50
rikeda-cloud <longsichitian648@gmail.com>
2023-10-23 16:43:17 +0900

    [add] statmachine_diagramをdictとlist別に分割した

--- src/parse/json/convert_json_to_json_object.c
+++ src/parse/json/convert_json_to_json_object.c
@@ -6,7 +6,7 @@
 /*   By: rikeda <rikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 18:42:21 by rikeda            #+#    #+#             */
-/*   Updated: 2023/10/21 19:20:04 by rikeda           ###   ########.fr       */
+/*   Updated: 2023/10/23 16:20:34 by rikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,23 +25,22 @@ t_vla	*convert_json_to_json_object(const char *file)
 	return (json_object);
 }
 
-#include <stdio.h>
-#include <stdlib.h>
-int main(int argc, char **argv)
-{
-	t_vla		*json_object;
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main(int argc, char **argv)
+// {
+// 	t_vla		*json_object;
 
-	if (argc != 2)
-	{
-		printf("argc != 2\n");
-		return (EXIT_FAILURE);
-	}
-	json_object = convert_json_to_json_object(argv[1]);
-	if (json_object != NULL)
-	{
-		json_generator(json_object, 1);
-		return (EXIT_SUCCESS);
-	}
-	else
-		return (EXIT_FAILURE);
-}
+// 	if (argc != 3) {
+// 		printf("argc != 3\n");
+// 		return (EXIT_FAILURE);
+// 	}
+// 	json_object = convert_json_to_json_object(argv[1]);
+// 	if (json_object == NULL) {
+// 		return (atoi(argv[2]) == 0);
+// 	}
+// 	else {
+// 		json_generator(json_object, 1);
+// 		return (atoi(argv[2]) == 1);
+// 	}
+// }
