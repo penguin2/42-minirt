343e6c0c50310423070f63409e13508c278aaebc
taekklee <taekklee@student.42tokyo.jp>
2023-11-12 22:29:26 +0900

    [add] ray-tracing recursively

--- inc/material.h
+++ inc/material.h
@@ -6,20 +6,22 @@
 /*   By: taekklee <taekklee@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 19:43:31 by taekklee          #+#    #+#             */
-/*   Updated: 2023/11/13 17:10:36 by taekklee         ###   ########.fr       */
+/*   Updated: 2023/11/13 18:14:32 by taekklee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MATERIAL_H
 # define MATERIAL_H
 
-# include "libcolor.h"
+# include <stdbool.h>
 
 typedef struct s_material{
 	double	k_ambient;
 	double	k_diffuse;
 	double	k_specular;
 	double	k_shininess;
+	bool	is_reflective;
+	double	k_reflect;
 }	t_material;
 
 t_material	material_create(
--- src/structs/image/image_render_scene.c
+++ src/structs/image/image_render_scene.c
@@ -6,7 +6,7 @@
 /*   By: taekklee <taekklee@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 19:12:18 by taekklee          #+#    #+#             */
-/*   Updated: 2023/11/03 12:48:57 by taekklee         ###   ########.fr       */
+/*   Updated: 2023/11/12 22:13:42 by taekklee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 #include <mlx.h>
 #include <stddef.h>
 
-static int	_get_ray_color(t_scene *scene, t_ray ray);
+static int	_pixel_to_color(t_vec3 pixel, t_scene *scene);
 
 void	image_render_scene(void *mlx, t_image *image, t_scene *scene)
 {
@@ -33,8 +33,7 @@ void	image_render_scene(void *mlx, t_image *image, t_scene *scene)
 	pixel = scene->camera.viewport.upper_left;
 	while (row_cnt < image->height)
 	{
-		image_write_color(mlx, image, addr,
-			_get_ray_color(scene, ray_from_to(scene->camera.eye, pixel)));
+		image_write_color(mlx, image, addr, _pixel_to_color(pixel, scene));
 		++col_cnt;
 		addr += image->byte_per_pixel;
 		pixel = vec3_add(pixel, scene->camera.viewport.dw);
@@ -49,24 +48,13 @@ void	image_render_scene(void *mlx, t_image *image, t_scene *scene)
 	}
 }
 
-/**
- * @brief tracing the given ray,
- * 		if the ray hits a certain object, it calculates the color of that hit.
- *
- * @param scene information of the scene
- * @param ray given ray to trace
- * @return color of the hit if the ray hits a certain object,
- * 		otherwise the default color (black!?)
- */
-static int	_get_ray_color(t_scene *scene, t_ray ray)
+static int	_pixel_to_color(t_vec3 pixel, t_scene *scene)
 {
-	t_color		color;
-	t_hit		*hit;
-
-	hit = hit_new(ray, &scene->objects);
-	if (hit == NULL)
-		return (color_cast(color_black()));
-	color = hit_get_color(hit, &scene->objects, &scene->lights);
-	hit_free(hit);
-	return (color_cast(color));
+	return (color_cast(
+			ray_convert_to_color(
+				ray_from_to(scene->camera.eye, pixel),
+				scene,
+				0
+			)
+		));
 }
