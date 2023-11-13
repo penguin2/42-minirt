e39fa8abeae603adc850b5b3b16295187c053b47
taekklee <taekklee@student.42tokyo.jp>
2023-11-13 18:05:37 +0900

    [fix] max value of color in t_color: 255 -> 1.0

--- lib/libcolor/color_composite.c
+++ lib/libcolor/color_composite.c
@@ -6,7 +6,7 @@
 /*   By: taekklee <taekklee@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 15:50:36 by taekklee          #+#    #+#             */
-/*   Updated: 2023/11/05 15:51:39 by taekklee         ###   ########.fr       */
+/*   Updated: 2023/11/13 15:27:23 by taekklee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ t_color	color_composite(t_color color1, t_color color2)
 {
 	return (color_create(
 			color1.red * color2.red,
-			color1.blue * color2.blue,
-			color1.green * color2.green));
+			color1.green * color2.green,
+			color1.blue * color2.blue));
 }
