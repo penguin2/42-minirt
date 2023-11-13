e80aa389e4078ff82f420546388f2bb9143f53eb
taekklee <taekklee@student.42tokyo.jp>
2023-11-13 19:55:31 +0900

    [fix] color_mix: rgb not rbg

--- lib/libcolor/color_mix.c
+++ lib/libcolor/color_mix.c
@@ -6,7 +6,7 @@
 /*   By: taekklee <taekklee@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 22:05:54 by taekklee          #+#    #+#             */
-/*   Updated: 2023/11/12 22:10:00 by taekklee         ###   ########.fr       */
+/*   Updated: 2023/11/13 18:17:31 by taekklee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ t_color	color_mix(t_color color1, t_color color2, double t)
 {
 	return (color_create(
 			t * color1.red + (1 - t) * color2.red,
-			t * color1.blue + (1 - t) * color2.blue,
-			t * color1.green + (1 - t) * color2.green));
+			t * color1.green + (1 - t) * color2.green,
+			t * color1.blue + (1 - t) * color2.blue));
 }
