20e9916dc09a0c631974ae964ff41bba7e9060c8
taekklee <taekklee@student.42tokyo.jp>
2023-11-17 19:32:15 +0900

    [add] sphere_get_checkerboard_color

--- src/structs/sphere/sphere_get_checkerboard_color.c
+++ src/structs/sphere/sphere_get_checkerboard_color.c
@@ -6,16 +6,28 @@
 /*   By: taekklee <taekklee@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 01:23:23 by taekklee          #+#    #+#             */
-/*   Updated: 2023/11/17 01:23:47 by taekklee         ###   ########.fr       */
+/*   Updated: 2023/11/17 19:30:53 by taekklee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "define.h"
 #include "libcolor.h"
 #include "sphere.h"
+#include "utils.h"
+#include <math.h>
 
 t_color	sphere_get_checkerboard_color(const t_sphere *sphere, t_vec3 pos)
 {
-	(void)pos;
-	(void)sphere;
-	return (color_white());
+	const t_vec3	coord = vec3_sub(pos, sphere->center);
+	const double	radius_proj = sqrt(coord.z * coord.z + coord.x * coord.x);
+	double			phi;
+	double			theta;
+
+	phi = acos(coord.y / sphere->radius);
+	theta = acos(coord.z / radius_proj);
+	if (coord.x < 0)
+		theta = -theta;
+	if (is_odd_2d(phi * 10 / PI, theta * 10 / PI))
+		return (color_white());
+	return (color_black());
 }
