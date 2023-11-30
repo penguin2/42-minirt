79128b1f3fdac1f9e063f56beeb97c6d5bed9225
rikeda-cloud <longsichitian648@gmail.com>
2023-11-30 14:43:34 +0900

    [add] ft_vla_sort関数 -> ft_sort関数を使用する様に変更

--- src/parse/rt/convert_rt_to_rt_objects_array/utils/sort/sort_rt_object_options.c
+++ src/parse/rt/convert_rt_to_rt_objects_array/utils/sort/sort_rt_object_options.c
@@ -6,57 +6,30 @@
 /*   By: rikeda <rikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 16:09:50 by rikeda            #+#    #+#             */
-/*   Updated: 2023/11/18 15:54:14 by rikeda           ###   ########.fr       */
+/*   Updated: 2023/11/30 14:28:29 by rikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "parse.h"
+#include <stdbool.h>
 #include <stddef.h>
 
-static const char	*_get_option_key(const t_vla *rt_object, size_t idx)
+static bool	_cmp_option_string(const void *ptr1, const void *ptr2)
 {
-	const char	**strings = rt_object->array[idx];
+	const char	**strings1 = (const char **)ptr1;
+	const char	**strings2 = (const char **)ptr2;
+	const char	*option_string1 = strings1[OPTION_KEY_IDX];
+	const char	*option_string2 = strings2[OPTION_KEY_IDX];
 
-	return (strings[OPTION_KEY_IDX]);
-}
-
-/**
-* @brief Obtain the index of the VLA in which the parameter of
-*		 the object with the lowest value is stored.
-*		 The 0th index of rt_object is not optional,
-*		 but contains a required parameter, so if idx == 0, return 0
-*		 and does not change the order.
-*
-* @param rt_object vla of required parameters and optional parameters.
-* @param idx Index to start scanning when looking for the smallest value
-*			 from the VLA. Find the index that contains the smallest value
-*			 from this index to rt_object->size.
-*
-* @return Index storing the smallest value in rt_object.
-*/
-static size_t	_get_smallest_option(const t_vla *rt_object, size_t idx)
-{
-	size_t		smallest_idx;
-	const char	*smallest_option;
-
-	if (idx == 0)
-		return (0);
-	smallest_idx = idx;
-	smallest_option = _get_option_key(rt_object, idx++);
-	while (idx < rt_object->size)
-	{
-		if (0 < ft_strcmp(smallest_option, _get_option_key(rt_object, idx)))
-		{
-			smallest_option = _get_option_key(rt_object, idx);
-			smallest_idx = idx;
-		}
-		idx++;
-	}
-	return (smallest_idx);
+	return (ft_strcmp(option_string1, option_string2) < 0);
 }
 
 void	sort_rt_object_options(const t_vla *rt_object)
 {
-	ft_vla_sort(rt_object, _get_smallest_option);
+	ft_sort(
+		rt_object->array,
+		rt_object->size,
+		_cmp_option_string
+		);
 }
