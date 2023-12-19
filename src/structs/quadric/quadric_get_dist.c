f0448a48a9776255e10850155ad51824b39e52e8
taekklee <taekklee@student.42tokyo.jp>
2023-12-19 18:13:09 +0900

    [fix] quadric get dist

--- src/structs/quadric/quadric_get_dist.c
+++ src/structs/quadric/quadric_get_dist.c
@@ -6,12 +6,13 @@
 /*   By: taekklee <taekklee@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 01:09:50 by taekklee          #+#    #+#             */
-/*   Updated: 2023/12/18 21:55:08 by taekklee         ###   ########.fr       */
+/*   Updated: 2023/12/19 18:20:00 by taekklee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "quadric.h"
 #include "utils.h"
+#include <float.h>
 
 static bool	_solve_quadric_ray_equation(
 				const t_quadric *quadric,
@@ -24,9 +25,9 @@ bool	quadric_get_dist(const t_object *object, t_ray ray, double *dist)
 	double			roots[NUM_QUADRATIC_EQUATION_ROOTS];
 
 	if (!_solve_quadric_ray_equation(quadric, ray, roots)
-		|| !is_higher(roots[HIGHER_ROOT], 0))
+		|| !is_higher(roots[HIGHER_ROOT], SQRT_EPS))
 		return (false);
-	if (is_higher(roots[LOWER_ROOT], 0))
+	if (is_higher(roots[LOWER_ROOT], SQRT_EPS))
 		*dist = roots[LOWER_ROOT];
 	else
 		*dist = roots[HIGHER_ROOT];
@@ -49,5 +50,13 @@ static bool	_solve_quadric_ray_equation(
 		+ quadric->coeff_array[COEFF_D]
 		* quadric_to_ray.z - quadric->coeff_array[COEFF_E];
 
+	if (is_zero(a))
+	{
+		if (is_zero(b))
+			return (false);
+		roots[LOWER_ROOT] = -c / b;
+		roots[HIGHER_ROOT] = roots[LOWER_ROOT];
+		return (true);
+	}
 	return (solve_quadratic_equation(roots, b / 2 / a, c / a));
 }
