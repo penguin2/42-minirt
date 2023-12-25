7ceda9ff30091438c1ca04b4fbb3adb0000faa72
taekklee <taekklee@student.42tokyo.jp>
2023-12-25 22:17:32 +0900

    [fix] object_free: including frees for ppm_reader

--- src/structs/object/object_free.c
+++ src/structs/object/object_free.c
@@ -6,11 +6,12 @@
 /*   By: taekklee <taekklee@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 16:50:03 by taekklee          #+#    #+#             */
-/*   Updated: 2023/10/12 16:59:24 by taekklee         ###   ########.fr       */
+/*   Updated: 2023/12/25 22:14:00 by taekklee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "object.h"
+#include "ppm_reader.h"
 #include <stdlib.h>
 
 void	object_free(void *object)
@@ -18,5 +19,7 @@ void	object_free(void *object)
 	const t_object	*obj = object;
 
 	obj->free_ptr(obj->ptr);
+	ppm_reader_free(obj->texture_map);
+	ppm_reader_free(obj->bump_map);
 	free(object);
 }
